301d3d245d78d5228dfe5c28b272825c01326dd7
DenisDubinin <dadubinin@gmail.com>
2016-12-21 18:45:05 +0100

    init1

--- includes.h
+++ includes.h
@@ -8,5 +8,21 @@
 #include <vector>
 #include <sstream>
 
+// Math
+#include <math.h>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+// QT
+#include <QtWidgets/qapplication.h>
+#include <QtOpenGL/qgl.h>
 
 #endif /* includes_h */

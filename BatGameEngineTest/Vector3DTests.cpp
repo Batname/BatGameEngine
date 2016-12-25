26dbb0ea3dea62a6c5e00e641b41d1db99fd2e95
DenisDubinin <dadubinin@gmail.com>
2016-12-25 18:58:36 +0100

    vector3D tests

--- BatGameEngineTest/Vector3DTests.cpp
+++ BatGameEngineTest/Vector3DTests.cpp
@@ -15,3 +15,40 @@ TEST(Vector3D, Vector3DConstructor)
     EXPECT_FLOAT_EQ(0.0f, defaultConstructor.y);
     EXPECT_FLOAT_EQ(0.0f, defaultConstructor.z);
 }
+
+TEST(Vector3D, ScalarMultiplication)
+{
+    Vector3D vec(-7, 3, -4);
+    Vector3D result1 = 8 * vec;
+    Vector3D result2 = vec * 8;
+    
+    EXPECT_FLOAT_EQ(result1.x, -56);
+    EXPECT_FLOAT_EQ(result1.y, 24);
+    EXPECT_FLOAT_EQ(result1.z, -32);
+    
+    EXPECT_FLOAT_EQ(result1.x, result2.x);
+    EXPECT_FLOAT_EQ(result1.y, result2.y);
+    EXPECT_FLOAT_EQ(result1.z, result2.z);
+
+}
+
+TEST(Vector3D, AssigmentPlusEql)
+{
+    Vector3D source(5, 4, 3);
+    Vector3D another(1, -1, -5);
+    another += source;
+    EXPECT_FLOAT_EQ(another.x, 1.0f + 5.0f);
+    EXPECT_FLOAT_EQ(another.y, -1.0f + 4.0f);
+    EXPECT_FLOAT_EQ(another.z, 3.0f + -5.0f);
+}
+
+TEST(Vector3D, AssigmentMinusEql)
+{
+    Vector3D source(5, 4, 2);
+    Vector3D another(1, -1, 0);
+    another -= source;
+    EXPECT_FLOAT_EQ(another.x, 1.0f - 5.0f);
+    EXPECT_FLOAT_EQ(another.y, -1.0f - 4.0f);
+    EXPECT_FLOAT_EQ(another.z, 0.0f - 2.0f);
+}
+
--- Vector3D.inl.h
+++ Vector3D.inl.h
@@ -1,5 +1,37 @@
 #ifndef Vector3D_inl_h
 #define Vector3D_inl_h
 
+Vector3D Vector3D::operator+=(const Vector3D& right)
+{
+    x += right.x;
+    y += right.y;
+    z += right.z;
+    
+    return * this;
+}
+
+Vector3D Vector3D::operator-=(const Vector3D& right)
+{
+    x -= right.x;
+    y -= right.y;
+    z -= right.z;
+    
+    return * this;
+}
+
+Vector3D operator*(float scalar, const Vector3D& right)
+{
+    return Vector3D(right.x * scalar, right.y * scalar, right.z * scalar);
+}
+
+Vector3D operator*(const Vector3D& right, float scalar)
+{
+    return Vector3D(right.x * scalar, right.y * scalar, right.z * scalar);
+}
+
+Vector3D operator+(const Vector3D& left, const Vector3D& right)
+{
+    return Vector3D(left.x + right.x, left.y + right.y, left.z + right.z);
+}
 
 #endif /* Vector3D_inl_h */

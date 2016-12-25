777921216313b372c4a3b5f286c127d4342bd8ee
DenisDubinin <dadubinin@gmail.com>
2016-12-25 09:20:06 +0100

    return optimisation

--- Matrix2D.inl.h
+++ Matrix2D.inl.h
@@ -9,11 +9,10 @@ Matrix2D::Matrix2D(float r0c0, float r0c1, float r1c0, float r1c1) :
 
 Vector2D operator*(const Matrix2D& matrix, const Vector2D& vector)
 {
-    Vector2D temp;
-    temp.x = matrix.r0c0 * vector.x + matrix.r0c1 * vector.y;
-    temp.y = matrix.r1c0 * vector.x + matrix.r1c1 * vector.y;
-    
-    return temp;
+    return Vector2D(
+       matrix.r0c0 * vector.x + matrix.r0c1 * vector.y,
+       matrix.r1c0 * vector.x + matrix.r1c1 * vector.y
+    );
 }
 
 Matrix2D Matrix2D::rotate(float angleInRadians)
